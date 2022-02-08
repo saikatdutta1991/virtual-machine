35483725f733c527e01fdbb84fe9088d3cdf113d
Saikat Dutta <saikatdutta1991@gmail.com>
2022-02-08 18:19:44 +0530

    First commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: "all",
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  semi: true,
+  printWidth: 80,
+  useTabs: false,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+};
