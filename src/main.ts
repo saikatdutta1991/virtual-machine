d721d937c08e3d84ea305439f53c4faa6f595faa
Saikat Dutta <saikatdutta1991@gmail.com>
2022-02-09 23:41:13 +0530

    Run program

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import RandomAccessMemory from './random-access-memory';
+import CentralProcessingUnit from './central-processing-unit';
+import { InstructionCode } from './enums/instruction-code';
+import { RegisterIdentifier } from './enums/registers-identifier';
+import RandomAccessMemory from './memories/random-access-memory';
 
 const ram = new RandomAccessMemory(64);
-ram.write(0, 100);
-ram.write(31, 1);
-ram.write(47, 1);
-ram.write(63, 1);
+ram.write(0x00, InstructionCode.MOV);
+ram.write(0x01, 0x12);
+ram.write(0x02, 0x34);
+ram.write(0x03, RegisterIdentifier.R1);
 
-// console.log(ram.read(1), ram.read(62));
-console.log(ram.toString());
+ram.write(0x04, InstructionCode.MOV);
+ram.write(0x05, 0xab);
+ram.write(0x06, 0xcd);
+ram.write(0x07, RegisterIdentifier.R2);
+
+const cpu = new CentralProcessingUnit(ram);
+cpu.cycle();
+cpu.cycle();
+cpu.debugRam();
+cpu.debugRegisters();
